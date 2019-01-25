d98ddc77be1291e8c7d53bc011aa4542106306a2
Jeongho Park <qkrwjdgh0904@naver.com>
2019-01-25 15:29:16 +0900

    init

--- baekjoon/Dynamic Programming 1/연속합_1912.cpp
+++ baekjoon/Dynamic Programming 1/연속합_1912.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 
-int answer(void){
+long long answer(void){
 	
-	int a[1001] = {0, };
-	int d1[1001] = {0, };
-	int d2[1001] = {0, }; 
-	int d[1001] = {0, };
-
-	int answer=0; 
+	int a[100001] = {0, };
+	long long d[100001] = {0, };
+	 
+	long long answer=0; 
 	int n;
 	cin>>n;
 	
@@ -20,31 +18,17 @@ int answer(void){
 		a[i] = num;
 	}
 	
-	//Αυ°‘	 
 	for(int i=1; i<=n; ++i){
-		d1[i]=1;
-		for(int j=1; j<=i; ++j){
-			if( a[i] > a[j] && d1[i] < d1[j]+1 )
-				d1[i] = d1[j]+1;
-		}
+		d[i] = a[i];
+		if( d[i] < d[i-1]+a[i] )
+			d[i] = d[i-1]+a[i]; 
 	}
 	
-	for(int i=n; i>=1; --i){
-		d2[i]=1;
-		for(int j=n; j>=i; --j){
-			if( a[i] > a[j] && d2[i] < d2[j]+1 )
-				d2[i] = d2[j]+1;
-		}
-	}
-	
+	answer = d[1];
 	for(int i=1; i<=n; ++i){
-		d[i] = d1[i]+d2[i]-1;
-		if( answer < d[i] )
+		if(answer < d[i])
 			answer = d[i];
 	}
-
-	
-	
 	return answer;
 	
 }

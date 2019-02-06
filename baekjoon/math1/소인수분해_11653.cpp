6e0650ca69eed5b74675741df76a288c5af4d195
Jeongho Park <qkrwjdgh0904@naver.com>
2019-02-06 14:33:40 +0900

    init

---
+++ baekjoon/math1/소인수분해_11653.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int answer(){
+	int n;
+	cin>>n;
+	
+	for(int i=2; i*i<=n; ++i){
+		while( n%i==0 ){
+			cout<<i<<'\n';
+			n/=i;
+		}
+	}
+	if(n>1){
+		cout<<n<<'\n';
+	}
+	
+}
+
+int main(void){
+	answer();
+	return 0;
+}

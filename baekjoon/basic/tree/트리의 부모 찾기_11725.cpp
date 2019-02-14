597d32c16e5d6e51dbda3798c4c48589eb44f54a
Jeongho Park <qkrwjdgh0904@naver.com>
2019-02-14 12:45:32 +0900

    init

---
+++ baekjoon/basic/tree/트리의 부모 찾기_11725.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+vector<int> v[100001];
+vector<int> p(100001);
+vector<bool> check(100001);
+void BFS(int node){
+	check[node] = true;
+	for(int i=0; i<v[node].size(); ++i){
+		int x = v[node][i];
+		if( check[x]==false ){
+			p[x] = node;
+			BFS(x);
+		}
+	}
+	
+}
+int main(){
+	int n;
+	scanf("%d", &n);
+	
+	for(int i=1; i<n; ++i){
+		int n1, n2;
+		cin>>n1>>n2;
+		v[n1].push_back(n2);
+		v[n2].push_back(n1);	
+	}
+	
+	BFS(1);
+	for(int i=2; i<=n; ++i){
+		printf("%d\n", p[i]);
+	}
+	
+
+	return 0;
+}

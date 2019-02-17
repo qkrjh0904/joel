f49652062296282f07f76b301acee3e4fdc0f2dd
Jeongho Park <qkrwjdgh0904@naver.com>
2019-02-17 23:35:25 +0900

    init

---
+++ java/Hello2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello2 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int num[] = {1,2,3,4,5};
+		int sum=0;
+		for(int k:num)
+			sum += k;
+		System.out.println("гую╨" + sum);
+		
+	}
+}

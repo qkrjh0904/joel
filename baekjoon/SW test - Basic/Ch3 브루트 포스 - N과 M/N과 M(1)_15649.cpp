01bb44bf8709a30b9871b41e87660602f54908d8
Jeongho Park <qkrwjdgh0904@naver.com>
2019-03-05 21:19:56 +0900

    joel

---
+++ baekjoon/SW test - Basic/Ch3 브루트 포스 - N과 M/N과 M(1)_15649.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> check(10);
+vector<int> a(10);
+
+void go(int index, int n, int m){
+	if( index==m ){
+		for(int i=0; i<m; ++i)
+			cout<<a[i]<<' ';
+		cout<<'\n';
+		return;
+	}
+	for(int i=1; i<=n; ++i){
+		if(check[i]) continue;
+		check[i] = true;
+		a[index] = i;
+		go(index+1, n, m);
+		check[i] = false;
+	}
+}
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	go(0, n, m);
+	
+	
+	
+	
+	return 0;
+}

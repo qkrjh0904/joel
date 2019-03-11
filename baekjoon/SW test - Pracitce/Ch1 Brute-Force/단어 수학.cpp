33b3511dee46669144abf952196d85e701d7b8eb
Jeongho Park <qkrwjdgh0904@naver.com>
2019-03-11 20:12:02 +0900

    joel

--- baekjoon/SW test - Pracitce/Ch1 Brute-Force/단어 수학.cpp
+++ baekjoon/SW test - Pracitce/Ch1 Brute-Force/단어 수학.cpp
@@ -49,64 +49,6 @@ void solution(){
 	cout<<answer<<endl;
 }
 
-void sol(){
-	int n;
-	cin>>n;
-	vector<string> input(n);
-	vector<int> input_length(n);
-	vector<pair<char, int>> map;
-	int total=0;
-	for(int i=0; i<n; ++i){
-		cin>>input[i];
-		input_length[i] = input[i].length();
-		total += input_length[i];
-	}
-	int number=9;
-	while(total!=0){
-		int big = input_length[0];
-		int cnt=0;
-		for(int i=0; i<input_length.size(); ++i){
-			if( big<input_length[i] ){
-				big = input_length[i];
-				cnt = i;
-			}
-		}
-		
-//		cout<<"big : "<<big<<" cnt : "<<cnt<<endl;
-//		cout<<input[cnt].length()-input_length[cnt]<<endl;
-		bool set=true;
-		for(int i=0; i<map.size(); ++i){
-			if(map[i].first == input[cnt][ input[cnt].length()-input_length[cnt] ])
-				set = false;
-		}
-		if(set){
-			map.push_back(make_pair(input[cnt][ input[cnt].length()-input_length[cnt] ], number--));
-//			cout<<input[cnt][ input[cnt].length()-input_length[cnt] ]<<" enter"<<endl;
-		}
-		input_length[cnt]--;
-		total--;
-	}
-//	for(int i=0; i<map.size(); ++i){
-//		cout<<map[i].first<<' '<<map[i].second<<'\n';
-//	}
-	
-	int answer=0;
-	for(int i=0; i<input.size(); ++i){
-		int leng = input[i].length();
-		int times=1;
-		for(int j=leng-1; j>=0; --j){
-			for(int k=0; k<map.size(); ++k){
-				if( map[k].first==input[i][j] ){
-					answer += map[k].second*times;
-					break;
-				}
-			}
-			times *= 10;
-		}
-	}
-	cout<<answer<<endl;
-	
-}
 int main(){
 	solution();
 	return 0;

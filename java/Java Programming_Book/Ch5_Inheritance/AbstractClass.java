90ef2b4b680f58a64785efdf45633faac7dc4f45
Jeongho Park <qkrwjdgh0904@naver.com>
2019-04-06 16:48:11 +0900

    abstract comment 추가

--- java/Java Programming_Book/Ch5_Inheritance/AbstractClass.java
+++ java/Java Programming_Book/Ch5_Inheritance/AbstractClass.java
@@ -1,3 +1,10 @@
+/*
+ * 추상클래스는 인스턴스를 생성할 수 없다.
+ * 그러므로 추상클래스는 인스턴스를 생성할 목적으로 만들지 않는다.
+ * 따라서 상속받는 서브클래스에서 강제로 오버라이딩하도록 지시하기위해 만든것이다.
+ * 서브클래스가 추상클래스가 되지 않기 위해서 추상메소드를 모두 오버라이딩 해야한다.
+ */
+
 abstract class Calculator{
 	public abstract int add(int a, int b);
 	public abstract int subtract(int a, int b);
@@ -25,7 +32,7 @@ public class AbstractClass {
 		Calculator c = new GoodCalc();
 		System.out.println(c.add(2, 3));
 		System.out.println(c.subtract(2, 3));
-		System.out.println(c.average(new int [] {2,3,4}));
+		System.out.println(c.average(new int [] {2,3,4})); 
 		
 
 	}
